9c9f65d6b657e3d69de5a63af1770973add82d53
URIEL_ISAI_AVILA_ROMAN <a.20308051280483@cbtis128.edu.mx>
2023-04-19 13:39:00 +0000

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,92 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Flutter Learning',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("Cards en Flutter"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Card(
+        child: Container(
+          height: 100,
+          color: Colors.white,
+          child: Row(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Expanded(
+                    child: Image.asset("images/card.jpg"),
+                    flex: 2,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: ListTile(
+                          title: Text("Pulp Fiction"),
+                          subtitle: Text("Quentin Tarantino"),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: Text("Ver"),
+                              onPressed: () {},
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            TextButton(
+                              child: Text("Sinopsis"),
+                              onPressed: () {},
+                            ),
+                            SizedBox(
+                              width: 8,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                flex: 8,
+              ),
+            ],
+          ),
         ),
+        elevation: 8,
+        margin: EdgeInsets.all(10),
       ),
     );
   }
